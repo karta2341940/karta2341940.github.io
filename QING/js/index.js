3816c543b13043c01c4a0be3ec468be6807bc0e6
karta2341940 <a08487a@gmail.com>
2020-09-04 11:54:31 +0800

    1.0.7-1

--- QING/js/index.js
+++ QING/js/index.js
@@ -3,4 +3,41 @@ function bgplay()
     let vd = document.getElementById("vd");
     vd.currentTime = 6;
 }
-console.log("Version : 1.0.7");
+function barblack()
+{
+    let bar=document.getElementById("topbar");
+    const barblack =(bar.offsetTop);
+    if(window.scrollY > 0)
+    {
+        bar.classList.add("bg-dark");
+    }
+    else if(window.scrollY == 0)
+    {
+        bar.classList.remove("bg-dark");
+    }
+}
+window.addEventListener("scroll",barblack);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Version : 1.0.7-1");
